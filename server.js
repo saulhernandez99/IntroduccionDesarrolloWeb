548f869a52ee27653a0af064b322af8661546116
Saúl Hernández <47616009+saulhernandez99@users.noreply.github.com>
2024-08-08 22:04:45 -0600

    Creacion de  server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+//Punto 6
+const http = require('http')
+const server = http.createServer((peticion, respuesta) => {
+    respuesta.writeHead(200, {'Content-Type': 'text/plain'})
+    respuesta.end("Hola mundo")
+})
+server.listen(3000)
